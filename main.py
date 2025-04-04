aee5542077bfb31a83486f6195322ab492cbf487
yonatanvolog <yonatan.vologdin@gmail.com>
2025-04-04 03:21:32 +0300

    Computed proxy according to the second paper

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from proxy_mutual_information_privbayes import ProxyMutualInformationPrivbayes
+from proxy_mutual_information_nist_contest import ProxyMutualInformationNistContest
 from mutual_information import MutualInformation
 
 if __name__ == "__main__":
     MutualInformation().calculate("sex", "income")
     ProxyMutualInformationPrivbayes().calculate("sex", "income")
+    ProxyMutualInformationNistContest().calculate("sex", "income")
 
     MutualInformation().calculate("race", "income")
     ProxyMutualInformationPrivbayes().calculate("race", "income")
+    ProxyMutualInformationNistContest().calculate("race", "income")
